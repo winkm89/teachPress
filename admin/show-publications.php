5927a393f5406675516ff8c963c6ed09963601e6
winkm89 <michael.mtrv@gmail.com>
2023-07-25 00:05:53 +0200

    Changes on edit publication links
    * Introduce using of target=blank for edit publication links
    * unused parameters from edit links removed

--- admin/show-publications.php
+++ admin/show-publications.php
@@ -311,11 +311,11 @@ class TP_Publications_Page {
         }
         TP_HTML::line('<th class="check-column"><input name="checkbox[]" class="tp_checkbox" type="checkbox" ' . $checked . ' value="' . $row->pub_id . '" title="' . __('Select the entry','teachpress') . '" /></th>');
         TP_HTML::line('<td>');
-        echo '<a href="admin.php?page=teachpress/addpublications.php&amp;pub_id=' . $row->pub_id . $get_string . '" class="teachpress_link" title="' . __('Click to edit','teachpress') . '"><strong>' . TP_HTML::prepare_title($row->title, 'decode') . '</strong></a>';
+        echo '<a href="admin.php?page=teachpress/addpublications.php&amp;pub_id=' . $row->pub_id . '" target="_blank" class="teachpress_link" title="' . __('Click to edit','teachpress') . '"><strong>' . TP_HTML::prepare_title($row->title, 'decode') . '</strong></a>';
         if ( $row->status === 'forthcoming' ) {
             echo '<span class="tp_pub_label_status">' . __('Forthcoming','teachpress') . '</span>';
         }
-        echo '<div class="tp_row_actions"><a href="admin.php?page=teachpress/addpublications.php&amp;pub_id=' . $row->pub_id . $get_string . '" class="teachpress_link" title="' . __('Click to edit','teachpress') . '">' . __('Edit','teachpress') . '</a> | <a href="' . admin_url( 'admin-ajax.php' ) . '?action=teachpress&cite_id=' . $row->pub_id . '" class="teachpress_cite_pub teachpress_link">' . __('Cite', 'teachpress') . '</a> | <a class="tp_row_delete" href="admin.php?page=' . $array_variables['page'] . '&amp;orderby=' . $array_variables['order'] . '&amp;checkbox%5B%5D=' . $row->pub_id . '&amp;action=delete' . $get_string . '" title="' . __('Delete','teachpress') . '">' . __('Delete','teachpress') . '</a></div>';
+        echo '<div class="tp_row_actions"><a href="admin.php?page=teachpress/addpublications.php&amp;pub_id=' . $row->pub_id . '" class="teachpress_link" target="_blank" title="' . __('Click to edit','teachpress') . '">' . __('Edit','teachpress') . '</a> | <a href="' . admin_url( 'admin-ajax.php' ) . '?action=teachpress&cite_id=' . $row->pub_id . '" class="teachpress_cite_pub teachpress_link">' . __('Cite', 'teachpress') . '</a> | <a class="tp_row_delete" href="admin.php?page=' . $array_variables['page'] . '&amp;orderby=' . $array_variables['order'] . '&amp;checkbox%5B%5D=' . $row->pub_id . '&amp;action=delete' . $get_string . '" title="' . __('Delete','teachpress') . '">' . __('Delete','teachpress') . '</a></div>';
         TP_HTML::line('</td>');
         TP_HTML::line('<td>' . $row->pub_id . '</td>');
         TP_HTML::line('<td>' . tp_translate_pub_type($row->type) . '</td>');
